d4d346827e42a90ddecd22d0f6952473480654c6
Bob W. Hogg <rwhogg@linux.com>
2016-05-06 14:00:42 -0400

    presentproto: Build a bottle for Linuxbrew

--- presentproto.rb
+++ presentproto.rb
@@ -1,3 +1,4 @@
+# presentproto: Build a bottle for Linuxbrew
 class Presentproto < Formula
   desc "X.Org Protocol Headers: presentproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
