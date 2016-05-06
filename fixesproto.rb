d792315e3e35482b93a66616bccd3d59c87f264a
Bob W. Hogg <rwhogg@linux.com>
2016-05-05 22:10:20 -0400

    fixesproto: Build a bottle for Linuxbrew

--- fixesproto.rb
+++ fixesproto.rb
@@ -1,3 +1,4 @@
+# fixesproto: Build a bottle for Linuxbrew
 class Fixesproto < Formula
   desc "X.Org Protocol Headers: fixesproto"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
