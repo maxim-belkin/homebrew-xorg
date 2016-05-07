b9a69edce04574a7a74f981a7a9ee094cd3e294f
Bob W. Hogg <rwhogg@linux.com>
2016-05-07 09:05:29 -0400

    libice: Build a bottle for Linuxbrew

--- libice.rb
+++ libice.rb
@@ -1,3 +1,4 @@
+# libice: Build a bottle for Linuxbrew
 class Libice < Formula
   desc "X.Org Libraries: libICE"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
