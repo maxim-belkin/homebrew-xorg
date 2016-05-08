62e813595d88bbaaf7d0ae637aaf89f2ecd95c56
Bob W. Hogg <rwhogg@linux.com>
2016-05-08 07:43:54 -0400

    libxaw: Build a bottle for Linuxbrew

--- libxaw.rb
+++ libxaw.rb
@@ -1,3 +1,4 @@
+# libxaw: Build a bottle for Linuxbrew
 class Libxaw < Formula
   desc "X.Org Libraries: libXaw"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
