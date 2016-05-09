26559b3e8cf314e0b6ead7e3d511755abd8cb23b
Bob W. Hogg <rwhogg@linux.com>
2016-05-08 22:07:21 -0400

    libxft: Build a bottle for Linuxbrew

--- libxft.rb
+++ libxft.rb
@@ -1,3 +1,4 @@
+# libxft: Build a bottle for Linuxbrew
 class Libxft < Formula
   desc "X.Org Libraries: libXft"
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
