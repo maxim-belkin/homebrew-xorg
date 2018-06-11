9b62a5b1d41f8186fbc9fb49cd7ae41eb98ed092
Gwang-Jin Lee <36105353+Gwang-Jin@users.noreply.github.com>
2018-06-11 10:28:44 +0900

    Update mesa.rb

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -25,6 +25,7 @@ class Mesa < Formula
   depends_on "linuxbrew/xorg/xextproto"
   depends_on "linuxbrew/xorg/xf86vidmodeproto"
   depends_on "linuxbrew/xorg/fixesproto"
+  depends_on "linuxbrew/xorg/videoproto"
 
   depends_on "linuxbrew/xorg/libdrm"
   depends_on "systemd" # provides libudev <= needed by "gbm"
