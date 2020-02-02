0170a07a804bf7571270b7a6c2e99d0df1f58c12
Maxim Belkin <maxim.belkin@gmail.com>
2020-02-02 20:39:15 +0000

    Switch to meson & ninja

--- Formula/libglvnd.rb
+++ Formula/libglvnd.rb
@@ -10,37 +10,31 @@ class Libglvnd < Formula
     sha256 "29da7d985d172c265c4b9573fc4a8cc6d29cf81f90f19840b18114942ff20289" => :x86_64_linux
   end
 
-  option "without-asm", "Build without assembly"
-  option "without-tls", "Build without TLS support"
-
   depends_on "linuxbrew/xorg/libpthread-stubs" => :build
   depends_on "linuxbrew/xorg/libxext" => :build
   depends_on "linuxbrew/xorg/util-macros" => :build
   depends_on "linuxbrew/xorg/xorgproto" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python@2" => :build
-
-  if build.head?
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
   depends_on "linuxbrew/xorg/libx11"
 
   def install
     args = %W[
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --localstatedir=#{var}
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --enable-asm=#{build.with?("asm") ? "yes" : "no"}
-      --enable-tls=#{build.with?("tls") ? "yes" : "no"}
+      -Dprefix=#{prefix}
+      -Dsysconfdir=#{etc}
+      -Dlocalstatedir=#{var}
+      -Dasm=enabled
+      -Dx11=enabled
+      -Dglx=enabled
+      -Dtls=enabled
     ]
 
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *args
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 end
