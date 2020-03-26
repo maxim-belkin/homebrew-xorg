e6bba7ee12f243c70759c29218a2425c6fca3911
Maxim Belkin <maxim.belkin@gmail.com>
2020-03-26 00:09:06 +0000

    xkeyboardconfig 2.29

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -1,22 +1,30 @@
 class Xkeyboardconfig < Formula
   desc "Keyboard configuration database for the X Window System"
-  homepage "https://xorg.freedesktop.org"
-  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.28.tar.bz2"
-  sha256 "69adb25b0fc64e4075f8ec0eab8d869892419f474f91fb69db1713de2062bdce"
+  homepage "https://www.freedesktop.org/wiki/Software/XKeyboardConfig/"
+  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.29.tar.bz2"
+  sha256 "1d4175278bf06000683656763a8b1d3282c61a314b6db41260c8efe92d621802"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "6abf9e8fc92ee379b7adf698492852d3d794189564f06976371ff63fd6c17a0a" => :x86_64_linux
   end
 
+  head do
+    url "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git"
+  end
+
   depends_on "gettext" => :build
   depends_on "intltool" => :build
   depends_on "libxslt" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => [:build, :test]
 
   def install
     # Needed by intltool (xml::parser)
     ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5"
+
+    # Fixed in the next release:
+    # https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config/-/commit/c847c834
+    inreplace "rules/compat/map-variants.py", /match\[[12]\]/, 'match.group(\1)'
 
     args = %W[
       --prefix=#{prefix}
@@ -31,5 +39,11 @@ class Xkeyboardconfig < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_predicate man7/"xkeyboard-config.7", :exist?
+    assert_equal "#{share}/X11/xkb", shell_output("pkg-config --variable=xkb_base xkeyboard-config").chomp
+    assert_match "Language: en_GB", shell_output("strings #{share}/locale/en_GB/LC_MESSAGES/xkeyboard-config.mo")
   end
 end
