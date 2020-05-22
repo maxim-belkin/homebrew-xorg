e73c11e11b1d00bf20771e9bb0fed254c5bfd4ae
Michka Popoff <michkapopoff@gmail.com>
2020-05-22 14:20:27 +0200

    xdriinfo: bump revision for mesa

--- Formula/xdriinfo.rb
+++ Formula/xdriinfo.rb
@@ -5,10 +5,11 @@ class Xdriinfo < Formula
   url "https://www.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
   mirror "https://ftp.x.org/pub/individual/app/xdriinfo-1.0.6.tar.bz2"
   sha256 "d9ccd2c3e87899417acc9ea1f3e319a4198112babe1dc711273584f607449d51"
+  revision 1
+
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "680bea023686a32b12077bc0c18b64875364bdbe08ee11c63b529fcf8a3b4a22" => :x86_64_linux
   end
 
   # tag "linuxbrew"
