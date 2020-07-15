41fa2875bf9a8758f9de597ae5388fd253cb5249
Bob "Wombat" Hogg <wombat@rwhogg.site>
2020-07-15 12:25:11 -0400

    xhost: add a test (#666)
    Possible messages:
    
    1) access control enabled, only authorized clients can connect
    2) access control disabled, clients can connect from any host

--- Formula/xhost.rb
+++ Formula/xhost.rb
@@ -1,5 +1,5 @@
 class Xhost < Formula
-  desc "X.Org Applications: xhost"
+  desc "X server access control tool"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xhost-1.0.8.tar.bz2"
@@ -35,5 +35,9 @@ class Xhost < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_match /access control (enabled|disabled)/, shell_output("xhost")
   end
 end
