1e63d3e78c05e69450f6e59afd8a4a2b34724913
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-05 20:30:48 +0000

    font-bitstream-type1: update 1.0.3_3 bottle.

--- Formula/font-bitstream-type1.rb
+++ Formula/font-bitstream-type1.rb
@@ -10,6 +10,8 @@ class FontBitstreamType1 < Formula
   # tag "linuxbrew"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    sha256 "b4151dd2aaea0a42aa4848b26d245ba4eb35164e329355214430c0fd2709ce04" => :x86_64_linux
   end
 
   keg_only "part of Xorg-fonts package"
