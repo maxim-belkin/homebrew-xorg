c4daec14226ac5e25b832f24fb8334fce26f1baa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-05 20:30:38 +0000

    font-bh-type1: update 1.0.3_2 bottle.

--- Formula/font-bh-type1.rb
+++ Formula/font-bh-type1.rb
@@ -10,6 +10,8 @@ class FontBhType1 < Formula
   # tag "linuxbrew"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    sha256 "092f778f97862d65dbb9d641cdc4c6876b38846220545135d6f75c8077375471" => :x86_64_linux
   end
 
   keg_only "part of Xorg-fonts package"
