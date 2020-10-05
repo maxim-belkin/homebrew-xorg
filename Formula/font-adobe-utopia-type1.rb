6c3415b9385a2f55479f1a47572d0c83d44f6957
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-05 20:39:00 +0000

    font-adobe-utopia-type1: update 1.0.4_2 bottle.

--- Formula/font-adobe-utopia-type1.rb
+++ Formula/font-adobe-utopia-type1.rb
@@ -10,6 +10,9 @@ class FontAdobeUtopiaType1 < Formula
   # tag "linuxbrew"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-xorg"
+    rebuild 1
+    sha256 "e1b179de6f5cc3b4e4b88ec8e1d87471abe8ddbc6dab9c01039d5de7ed04c2ad" => :x86_64_linux
   end
 
   keg_only "part of Xorg-fonts package"
