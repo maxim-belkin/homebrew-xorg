4d080cc8e88ae37c0b145aa05aae868f04d8cb08
Michka Popoff <michkapopoff@gmail.com>
2020-10-10 22:49:21 +0200

    libdrm: rebuild for libpciaccess

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -3,6 +3,7 @@ class Libdrm < Formula
   homepage "https://dri.freedesktop.org"
   url "https://dri.freedesktop.org/libdrm/libdrm-2.4.102.tar.xz"
   sha256 "8bcbf9336c28e393d76c1f16d7e79e394a7fce8a2e929d52d3ad7ad8525ba05b"
+  revision 1
 
   livecheck do
     url "https://dri.freedesktop.org/libdrm/"
@@ -11,7 +12,6 @@ class Libdrm < Formula
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 "9a319753439896cd535fd716b2effb248b2f7f28c8eef3305d18303ba4002264" => :x86_64_linux
   end
 
   option "without-test", "Skip compile-time tests"
@@ -22,7 +22,7 @@ class Libdrm < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libpciaccess"
+  depends_on "libpciaccess"
 
   def install
     mkdir "build" do
