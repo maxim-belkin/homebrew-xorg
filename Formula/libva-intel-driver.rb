dbea9b435182e0b6e5d17b025c32eec45828901f
Michka Popoff <michkapopoff@gmail.com>
2020-10-16 22:05:17 +0200

    libdrm: migrate to homebrew-core

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -3,6 +3,7 @@ class LibvaIntelDriver < Formula
   homepage "https://github.com/intel/intel-vaapi-driver"
   url "https://github.com/intel/intel-vaapi-driver/releases/download/2.4.0/intel-vaapi-driver-2.4.0.tar.bz2"
   sha256 "71e2ddd985af6b221389db1018c4e8ca27a7f939fb51dcdf49d0efcb5ff3d089"
+  revision 1
 
   livecheck do
     url "https://github.com/intel/intel-vaapi-driver/releases"
@@ -10,12 +11,11 @@ class LibvaIntelDriver < Formula
   end
 
   bottle do
-    sha256 "5d6cb7e16619d29f46d47d218304b8104f10c01b139470e298d98d6cc46faec5" => :x86_64_linux
   end
 
   depends_on "linuxbrew/xorg/libva" => :build
   depends_on "pkg-config" => :build
-  depends_on "linuxbrew/xorg/libdrm"
+  depends_on "libdrm"
   depends_on "linuxbrew/xorg/wayland"
 
   def install
