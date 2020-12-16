854bc3c40388eb43941561daeade3dd119f796cb
Michka Popoff <michkapopoff@gmail.com>
2020-12-16 07:33:34 +0100

    libva-intel-driver: use wayland from homebrew-core

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -3,7 +3,7 @@ class LibvaIntelDriver < Formula
   homepage "https://github.com/intel/intel-vaapi-driver"
   url "https://github.com/intel/intel-vaapi-driver/releases/download/2.4.0/intel-vaapi-driver-2.4.0.tar.bz2"
   sha256 "71e2ddd985af6b221389db1018c4e8ca27a7f939fb51dcdf49d0efcb5ff3d089"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://github.com/intel/intel-vaapi-driver/releases"
@@ -12,13 +12,12 @@ class LibvaIntelDriver < Formula
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 "82b23c9c0ab719124528b8754d3369a38e1a88c13d6e755b6c9a2fa6db145c82" => :x86_64_linux
   end
 
   depends_on "linuxbrew/xorg/libva" => :build
   depends_on "pkg-config" => :build
   depends_on "libdrm"
-  depends_on "linuxbrew/xorg/wayland"
+  depends_on "wayland"
 
   def install
     ENV["LIBVA_DRIVERS_PATH"] = lib
