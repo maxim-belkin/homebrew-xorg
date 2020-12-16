ac8f5ded878d29a99f70cecbf4f41cc1417c9be9
Michka Popoff <michkapopoff@gmail.com>
2020-12-16 07:32:57 +0100

    libva-utils: use wayland from homebrew-core

--- Formula/libva-utils.rb
+++ Formula/libva-utils.rb
@@ -3,18 +3,17 @@ class LibvaUtils < Formula
   homepage "https://github.com/01org/libva-utils"
   url "https://github.com/intel/libva-utils/releases/download/2.6.0/libva-utils-2.6.0.tar.bz2"
   sha256 "2249b5d08bffc3862bbdcc9a6a4827afd504330b8d101564d39fe1a1e7adc426"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "8295cd0ba2445ec42702e9600c951ae52552431f11ee9936e8077a3c51177475" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
   depends_on "linuxbrew/xorg/libva"
-  depends_on "linuxbrew/xorg/wayland"
+  depends_on "wayland"
 
   def install
     args = %W[
