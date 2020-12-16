de5b96379ad0868f04801a88fcdfb05b5f39ccf6
Michka Popoff <michkapopoff@gmail.com>
2020-12-16 07:32:26 +0100

    wayland-protocols: use wayland from homebrew-core

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -3,10 +3,10 @@ class WaylandProtocols < Formula
   homepage "https://wayland.freedesktop.org"
   url "https://wayland.freedesktop.org/releases/wayland-protocols-1.18.tar.xz"
   sha256 "3d73b7e7661763dc09d7d9107678400101ecff2b5b1e531674abfa81e04874b3"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a61d3992131e08d3d69a78b0018701e309c2a1ce341a4a98d9ca8b755ce30931" => :x86_64_linux
   end
 
   head do
@@ -15,8 +15,8 @@ class WaylandProtocols < Formula
 
   option "without-test", "Skip compile-time tests"
 
-  depends_on "linuxbrew/xorg/wayland" => :build
   depends_on "pkg-config" => :build
+  depends_on "wayland" => :build
 
   if build.head?
     depends_on "autoconf" => :build
