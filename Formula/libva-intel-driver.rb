a593f820bebb4c59a838e2630c7caf3ab1f918de
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-20 19:25:59 -0500

    libva-intel-driver: update livecheck

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -6,8 +6,8 @@ class LibvaIntelDriver < Formula
   revision 2
 
   livecheck do
-    url "https://github.com/intel/intel-vaapi-driver/releases"
-    regex(%r{latest.*?href="/intel/intel-vaapi-driver/tree/v?([a-z0-9.]+)}mi)
+    url :stable
+    strategy :github_latest
   end
 
   bottle do
