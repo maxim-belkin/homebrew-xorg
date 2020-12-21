7cbc8d4a79c43ea475938a8d13ac5e456f8bab77
Michka Popoff <michkapopoff@gmail.com>
2020-12-21 15:46:24 +0100

    xcb-util*: bump revisions for xcb-util migration

--- Formula/xcb-util-cursor.rb
+++ Formula/xcb-util-cursor.rb
@@ -3,12 +3,11 @@ class XcbUtilCursor < Formula
   homepage "https://xcb.freedesktop.org"
   url "https://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.3.tar.bz2"
   sha256 "05a10a0706a1a789a078be297b5fb663f66a71fb7f7f1b99658264c35926394f"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "5783dc42becf80a55702f12171bc0125d4fbf72380355b3d373c15b90ee568f8" => :x86_64_linux
   end
 
   option "with-docs", "Regenerate documentation (requires doxygen)"
@@ -18,8 +17,8 @@ class XcbUtilCursor < Formula
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "libxcb"
-  depends_on "linuxbrew/xorg/xcb-util-image"
-  depends_on "linuxbrew/xorg/xcb-util-renderutil"
+  depends_on "xcb-util-image"
+  depends_on "xcb-util-renderutil"
 
   def install
     args = %W[
