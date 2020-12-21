986f0445d8b0421230cc791332fa54b4f94fabe5
Michka Popoff <michkapopoff@gmail.com>
2020-12-21 15:46:24 +0100

    xcb-util*: bump revisions for xcb-util migration

--- Formula/xwininfo.rb
+++ Formula/xwininfo.rb
@@ -4,18 +4,17 @@ class Xwininfo < Formula
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/xwininfo-1.1.5.tar.bz2"
   sha256 "7a405441dfc476666c744f5fcd1bc8a75abf8b5b1d85db7b88b370982365080e"
-  revision 1
+  revision 2
   # tag "linuxbrew"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-xorg"
     cellar :any_skip_relocation
-    sha256 "dde853059c2e6c93025b0fe085be3478065c8a29db3539efa9a7a3832442324d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
-  depends_on "linuxbrew/xorg/xcb-util-wm"
+  depends_on "xcb-util-wm"
 
   def install
     args = %W[
