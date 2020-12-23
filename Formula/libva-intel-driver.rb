aa5046b9021f6c47ca3e048a9658206e2e0cc5ca
Jonathan Chang <me@jonathanchang.org>
2020-12-23 14:45:55 +1100

    libva-intel-driver: update deps for migrations

--- Formula/libva-intel-driver.rb
+++ Formula/libva-intel-driver.rb
@@ -3,7 +3,7 @@ class LibvaIntelDriver < Formula
   homepage "https://github.com/intel/intel-vaapi-driver"
   url "https://github.com/intel/intel-vaapi-driver/releases/download/2.4.0/intel-vaapi-driver-2.4.0.tar.bz2"
   sha256 "71e2ddd985af6b221389db1018c4e8ca27a7f939fb51dcdf49d0efcb5ff3d089"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
@@ -15,7 +15,7 @@ class LibvaIntelDriver < Formula
     sha256 "9f7b8fd12c0bcbda395f5d55ee980075ad309d5961b0bf4c831478715d5fb757" => :x86_64_linux
   end
 
-  depends_on "linuxbrew/xorg/libva" => :build
+  depends_on "libva" => :build
   depends_on "pkg-config" => :build
   depends_on "libdrm"
   depends_on "wayland"
