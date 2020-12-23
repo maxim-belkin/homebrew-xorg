bd9e6fd3c46d286223229d717df7a41c924adb47
Jonathan Chang <me@jonathanchang.org>
2020-12-23 14:45:55 +1100

    libva-utils: update deps for migrations

--- Formula/libva-utils.rb
+++ Formula/libva-utils.rb
@@ -3,7 +3,7 @@ class LibvaUtils < Formula
   homepage "https://github.com/01org/libva-utils"
   url "https://github.com/intel/libva-utils/releases/download/2.6.0/libva-utils-2.6.0.tar.bz2"
   sha256 "2249b5d08bffc3862bbdcc9a6a4827afd504330b8d101564d39fe1a1e7adc426"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
@@ -17,8 +17,8 @@ class LibvaUtils < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "libva"
   depends_on "libx11"
-  depends_on "linuxbrew/xorg/libva"
   depends_on "wayland"
 
   def install
