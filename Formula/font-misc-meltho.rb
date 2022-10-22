810c59961c31a50643e9d2c34cee96c2ab88aec4
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-misc-meltho: rebottle for ghcr.io

--- Formula/font-misc-meltho.rb
+++ Formula/font-misc-meltho.rb
@@ -7,19 +7,13 @@ class FontMiscMeltho < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-misc-meltho-1.0.3.tar.bz2"
   sha256 "3721323f13855cf7ca609115a1f7b182491e9b2b9c6e01eb1a2c7f8edd480791"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "209f4a6865d133d2ee5812705792f75fecfb2d099ae3a4a01c7057e27d6cb02a"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
