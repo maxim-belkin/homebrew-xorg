693e9264765e6750fb42e91b6ddbdc983d8fa7ef
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-ibm-type1: rebottle for ghcr.io

--- Formula/font-ibm-type1.rb
+++ Formula/font-ibm-type1.rb
@@ -7,19 +7,13 @@ class FontIbmType1 < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-ibm-type1-1.0.3.tar.bz2"
   sha256 "fddb28d3db5a07f4b4ca15388488a9680a10e1367a18f358f903b2a608a5d2df"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "c60c7688f98e7905f2e3a0eb14ee43e7b710ccca5dd821871514cdf7eef5f432"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
