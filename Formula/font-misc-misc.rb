c4ddc6236a4e5451a83ad5c111dfe1d64316995e
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-misc-misc: rebottle for ghcr.io

--- Formula/font-misc-misc.rb
+++ Formula/font-misc-misc.rb
@@ -7,20 +7,14 @@ class FontMiscMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-misc-misc-1.1.2.tar.bz2"
   sha256 "b8e77940e4e1769dc47ef1805918d8c9be37c708735832a07204258bacc11794"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "0d14a2ebc94f76db169edc50b1d308eb0fdb988fe2a0257b89e2e25e6589843f"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
