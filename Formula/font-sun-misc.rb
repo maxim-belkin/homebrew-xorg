1351ea882c8c2ab5d7618a7f5822574100766563
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:28 -0700

    font-sun-misc: rebottle for ghcr.io

--- Formula/font-sun-misc.rb
+++ Formula/font-sun-misc.rb
@@ -7,15 +7,14 @@ class FontSunMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-sun-misc-1.0.3.tar.bz2"
   sha256 "481f4fcbbf7005658b080b3cf342c8c76de752e77f47958b2b383de73266d2e0"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
