d013eec7cd9b91b862b9f693e4451fde4397bf45
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:34 -0700

    xlsclients: rebottle for ghcr.io

--- Formula/xlsclients.rb
+++ Formula/xlsclients.rb
@@ -6,12 +6,6 @@ class Xlsclients < Formula
   mirror "https://ftp.x.org/pub/individual/app/xlsclients-1.1.4.tar.bz2"
   sha256 "773f2af49c7ea2c44fba4213bee64325875c1b3c9bc4bbcd8dac9261751809c1"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5f492f6aca3337a62a5e78c809244c50fe7c3dfb617c40b0c34e86a0f3b43c87"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "libxcb"
