ace9ee97d0b61db5233cca4df648eb9af4e1021f
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:23 -0700

    font-adobe-utopia-100dpi: rebottle for ghcr.io

--- Formula/font-adobe-utopia-100dpi.rb
+++ Formula/font-adobe-utopia-100dpi.rb
@@ -7,20 +7,14 @@ class FontAdobeUtopia100dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-adobe-utopia-100dpi-1.0.4.tar.bz2"
   sha256 "d16f5e3f227cc6dd07a160a71f443559682dbc35f1c056a5385085aaec4fada5"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "bab3f88050d8e9e9377c028c5a639bee32b94618ea09a1401646428e83db2a17"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
