852febe7b3d2a4a6a096a5129978de8a874fd6e9
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:32 -0700

    xcmsdb: rebottle for ghcr.io

--- Formula/xcmsdb.rb
+++ Formula/xcmsdb.rb
@@ -6,12 +6,6 @@ class Xcmsdb < Formula
   mirror "https://ftp.x.org/pub/individual/app/xcmsdb-1.0.5.tar.bz2"
   sha256 "e5585361bb8b6a05bb814a8d0e444ee93e0f00180881d3070aff4571e97f67c6"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c82ab67d6693968a113ef2befca3c83fcaf782bb43525c9ec60c317de612dcb3"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
