6b17f425e45b9b6755b17f74274fec243b488d5c
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:23 -0700

    font-adobe-75dpi: rebottle for ghcr.io

--- Formula/font-adobe-75dpi.rb
+++ Formula/font-adobe-75dpi.rb
@@ -7,20 +7,14 @@ class FontAdobe75dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-adobe-75dpi-1.0.3.tar.bz2"
   sha256 "c6024a1e4a1e65f413f994dd08b734efd393ce0a502eb465deb77b9a36db4d09"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "448fbcf2dd37b83aa4f96f5fd7690c0228f3d28d258148764eaf3def5f3d3297"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
