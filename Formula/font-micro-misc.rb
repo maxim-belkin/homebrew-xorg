a6cffbd4f4c1fbd1370fc258f0ce01049705d57e
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-micro-misc: rebottle for ghcr.io

--- Formula/font-micro-misc.rb
+++ Formula/font-micro-misc.rb
@@ -7,20 +7,14 @@ class FontMicroMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-micro-misc-1.0.3.tar.bz2"
   sha256 "9a3381c10f32d9511f0ad4179df395914c50779103c16cddf7017f5220ed8db6"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "8d8180148f8e5afc60c4acb48bad85e0c179a9af3bae1484f4bbff2aa72aff6f"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
