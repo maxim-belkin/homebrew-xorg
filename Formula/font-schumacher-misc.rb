e4bd81d88641aa9d6fc945d63d5e7b64425acfe7
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:28 -0700

    font-schumacher-misc: rebottle for ghcr.io

--- Formula/font-schumacher-misc.rb
+++ Formula/font-schumacher-misc.rb
@@ -7,20 +7,14 @@ class FontSchumacherMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-schumacher-misc-1.1.2.tar.bz2"
   sha256 "e444028656e0767e2eddc6d9aca462b16a2be75a47244dbc199b2c44eca87e5a"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "02904e3a0c4e70b0c59543aba1b476f56ea04dc7b8b5119b83f6f94725395f41"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
