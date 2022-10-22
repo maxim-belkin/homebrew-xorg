08f73bcd2c74d95abe368f5735e7ee4deaed8438
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:37 -0700

    xwd: rebottle for ghcr.io

--- Formula/xwd.rb
+++ Formula/xwd.rb
@@ -6,12 +6,6 @@ class Xwd < Formula
   mirror "https://ftp.x.org/pub/individual/app/xwd-1.0.7.tar.bz2"
   sha256 "cd6815b8b9e0b98e284d3d732fb12162159cb9dcee4f45a7d4c0bd8b308a6794"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "16af978d892789d0a3fe568034f8740a7113ac8a1270d9df19ecdd9c5e1679aa"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "libx11"
