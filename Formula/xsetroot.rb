f75f311e29235436e271a9a4f0312e0ac8ba4fa4
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:37 -0700

    xsetroot: rebottle for ghcr.io

--- Formula/xsetroot.rb
+++ Formula/xsetroot.rb
@@ -6,12 +6,6 @@ class Xsetroot < Formula
   mirror "https://ftp.x.org/pub/individual/app/xsetroot-1.1.2.tar.bz2"
   sha256 "10c442ba23591fb5470cea477a0aa5f679371f4f879c8387a1d9d05637ae417c"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c3c945a829a7df41efab464fe6dabe72432fc0ca240fae141af6ba7802867297"
-  end
 
   # xmuu x11 xbitmaps xcursor xorgproto
   depends_on "pkg-config" => :build
