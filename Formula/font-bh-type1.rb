3cc27d3afa03f1d19509becfa2d44a58c91cff84
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:25 -0700

    font-bh-type1: rebottle for ghcr.io

--- Formula/font-bh-type1.rb
+++ Formula/font-bh-type1.rb
@@ -7,19 +7,13 @@ class FontBhType1 < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bh-type1-1.0.3.tar.bz2"
   sha256 "761455a297486f3927a85d919b5c948d1d324181d4bea6c95d542504b68a63c1"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "092f778f97862d65dbb9d641cdc4c6876b38846220545135d6f75c8077375471"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
