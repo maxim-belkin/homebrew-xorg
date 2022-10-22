fc901dc2e28737165a9cc1fde550095e40481b16
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:24 -0700

    font-bh-ttf: rebottle for ghcr.io

--- Formula/font-bh-ttf.rb
+++ Formula/font-bh-ttf.rb
@@ -7,19 +7,13 @@ class FontBhTtf < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bh-ttf-1.0.3.tar.bz2"
   sha256 "1b4bea63271b4db0726b5b52c97994c3313b6023510349226908090501abd25f"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "f8804a8c3334e568ee1c7abc5fd1687bc97d973c581b452d5021f274f1de52d6"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
