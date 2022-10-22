5ea29cc7723b270bf4d4d0852f87222f265cce67
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:31 -0700

    mtdev: rebottle for ghcr.io

--- Formula/mtdev.rb
+++ Formula/mtdev.rb
@@ -3,15 +3,10 @@ class Mtdev < Formula
   homepage "https://bitmath.org"
   url "https://bitmath.org/code/mtdev/mtdev-1.1.5.tar.bz2"
   sha256 "6677d5708a7948840de734d8b4675d5980d4561171c5a8e89e54adf7a13eba7f"
-  # tag "linuxbrew"
 
   livecheck do
     url "https://bitmath.org/code/mtdev/"
     regex(/href=.*?mtdev[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "32778f8b518f949bd06e4ec20c3d9d0915febaedfb01011c7ab936076d88cd51"
   end
 
   depends_on "pkg-config" => :build
