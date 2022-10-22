346062060ebc5361f90d9f160fbb53cf93831c56
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-daewoo-misc: rebottle for ghcr.io

--- Formula/font-daewoo-misc.rb
+++ Formula/font-daewoo-misc.rb
@@ -7,15 +7,14 @@ class FontDaewooMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-daewoo-misc-1.0.3.tar.bz2"
   sha256 "bc65de70bee12698caa95b523d3b652c056347e17b68cc8b5d6bbdff235c4be8"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
