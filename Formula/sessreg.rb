adaf4f992aa4eb7e560a6ba64bab0d28db880f76
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:31 -0700

    sessreg: rebottle for ghcr.io

--- Formula/sessreg.rb
+++ Formula/sessreg.rb
@@ -4,11 +4,6 @@ class Sessreg < Formula
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url "https://www.x.org/pub/individual/app/sessreg-1.1.2.tar.bz2"
   sha256 "dfcb9e954273ace63879aec053c7880466f8755752a35aa9ced3fa5f04fd2c33"
-  # tag "linuxbrew"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "317fee80371ba91a2fc2c9b42922c717a9ffc6bed04f3fcf6210cd8ff83b28a3"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
