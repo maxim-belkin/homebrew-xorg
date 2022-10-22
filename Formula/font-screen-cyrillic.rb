619a73bb9a76bb718ca0ca52a3bd404bde54690d
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:28 -0700

    font-screen-cyrillic: rebottle for ghcr.io

--- Formula/font-screen-cyrillic.rb
+++ Formula/font-screen-cyrillic.rb
@@ -7,15 +7,14 @@ class FontScreenCyrillic < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-screen-cyrillic-1.0.4.tar.bz2"
   sha256 "824231e8dffe15299454e47259f29d98001c9cf8ad3d6b5171399e4d71705e79"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
