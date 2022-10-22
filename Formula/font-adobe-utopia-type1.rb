a2e97fea91fc4b66c517ccfda603d754cce88622
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:23 -0700

    font-adobe-utopia-type1: rebottle for ghcr.io

--- Formula/font-adobe-utopia-type1.rb
+++ Formula/font-adobe-utopia-type1.rb
@@ -7,20 +7,13 @@ class FontAdobeUtopiaType1 < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-adobe-utopia-type1-1.0.4.tar.bz2"
   sha256 "979435105f897a70f8993fa02c8362160b0513366c2ab896965416f96dbb8077"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    rebuild 1
-    sha256 x86_64_linux: "e1b179de6f5cc3b4e4b88ec8e1d87471abe8ddbc6dab9c01039d5de7ed04c2ad"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
