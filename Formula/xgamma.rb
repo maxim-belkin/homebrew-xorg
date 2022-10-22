00367c6ae770e1d42c0ae38001b408c4556a6ec5
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:33 -0700

    xgamma: rebottle for ghcr.io

--- Formula/xgamma.rb
+++ Formula/xgamma.rb
@@ -6,12 +6,6 @@ class Xgamma < Formula
   mirror "https://ftp.x.org/pub/individual/app/xgamma-1.0.6.tar.bz2"
   sha256 "0ef1c35b5c18b1b22317f455c8df13c0a471a8efad63c89c98ae3ce8c2b222d3"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1afa4d7d867977f2744195d708b87463c17c6d9c4f6959a6aec726827f42a73d"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
