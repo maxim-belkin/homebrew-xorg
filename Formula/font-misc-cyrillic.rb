9b19fea1b54dd8760c2ac7e3139b43c9f17f48d0
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-misc-cyrillic: rebottle for ghcr.io

--- Formula/font-misc-cyrillic.rb
+++ Formula/font-misc-cyrillic.rb
@@ -7,15 +7,14 @@ class FontMiscCyrillic < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-misc-cyrillic-1.0.3.tar.bz2"
   sha256 "e40fe3e3323c62b738550795457ad555c70c008aa91b5912dfd46f8e745f5e60"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
