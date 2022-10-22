f7ecb01e41aa90413aa162e364243734d7eb8105
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:25 -0700

    font-cronyx-cyrillic: rebottle for ghcr.io

--- Formula/font-cronyx-cyrillic.rb
+++ Formula/font-cronyx-cyrillic.rb
@@ -7,15 +7,14 @@ class FontCronyxCyrillic < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-cronyx-cyrillic-1.0.3.tar.bz2"
   sha256 "6e8631936157677c77ba032b5c7b1fb3cb2ee872dbcea0444f12cd602cd9212a"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
