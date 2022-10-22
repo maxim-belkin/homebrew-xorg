06c4de5bfcc03fc3cb4f4c94916777189db6a76a
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:32 -0700

    xcursor-themes: rebottle for ghcr.io

--- Formula/xcursor-themes.rb
+++ Formula/xcursor-themes.rb
@@ -7,14 +7,8 @@ class XcursorThemes < Formula
   mirror "ftp://ftp.x.org/pub/individual/data/xcursor-themes-1.0.6.tar.bz2"
   sha256 "ee1ec574741293abcf66ac14ce7e74add7ac6be7deb8b38179ef010d22354999"
   revision 1
-  # tag "linuxbrew"
 
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7bc2942d8f405f0daed6d8fc63d71f569cf242874d3e150830cba9dfa06cece8"
-  end
-
-  depends_on "linuxbrew/xorg/xcursorgen" => :build
+  depends_on "maxim-belkin/xorg/xcursorgen" => :build
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
 
