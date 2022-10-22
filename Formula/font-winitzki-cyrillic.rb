4bb554fd8825f2b44e421498ea2d1414c6d8d347
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:28 -0700

    font-winitzki-cyrillic: rebottle for ghcr.io

--- Formula/font-winitzki-cyrillic.rb
+++ Formula/font-winitzki-cyrillic.rb
@@ -7,13 +7,12 @@ class FontWinitzkiCyrillic < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-winitzki-cyrillic-1.0.3.tar.bz2"
   sha256 "abd13b63d02fcaec488686c23683e5cf640b43bd32f8ca22eeae6f84df0a36a0"
   revision 2
-  # tag "linuxbrew"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
