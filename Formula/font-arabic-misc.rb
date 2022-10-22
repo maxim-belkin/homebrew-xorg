5a837f9d140d239d2f4474f5ec34c24ffe99ebbd
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:24 -0700

    font-arabic-misc: rebottle for ghcr.io

--- Formula/font-arabic-misc.rb
+++ Formula/font-arabic-misc.rb
@@ -7,18 +7,12 @@ class FontArabicMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-arabic-misc-1.0.3.tar.bz2"
   sha256 "505d9b12a7093389e67a925dfda6346bde26d114c67f0cdca7aeda6e5d3344f4"
   revision 2
-  # tag "linuxbrew"
 
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "454bf2de5aac3e472ca56b0391390c9f40b28780442c800a8ffbf498e95a48a0"
-  end
-
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
