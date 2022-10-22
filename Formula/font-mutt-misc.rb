0c80653b8ddbab38ddbc6e6ef67ccf19a9a666f0
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-mutt-misc: rebottle for ghcr.io

--- Formula/font-mutt-misc.rb
+++ Formula/font-mutt-misc.rb
@@ -7,20 +7,14 @@ class FontMuttMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-mutt-misc-1.0.3.tar.bz2"
   sha256 "bd5f7adb34367c197773a9801df5bce7b019664941900b2a31fbfe1ff2830f8f"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "b07c83e61f1f4e5966e148aac74b9e6ed8d0d2ddafb65882aca5015daab32451"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
