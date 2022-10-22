41775b77a2787934b6db24dccce01acde41aaa49
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:23 -0700

    font-adobe-100dpi: rebottle for ghcr.io

--- Formula/font-adobe-100dpi.rb
+++ Formula/font-adobe-100dpi.rb
@@ -7,20 +7,14 @@ class FontAdobe100dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-adobe-100dpi-1.0.3.tar.bz2"
   sha256 "b2c08433eab5cb202470aa9f779efefce8d9cab2534f34f3aa4a31d05671c054"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "93038f5244a80ba5e856ae85db957319be14a4792e569ce1ba9ecce93a42d81c"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
