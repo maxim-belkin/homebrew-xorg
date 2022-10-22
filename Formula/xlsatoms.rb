bb312c9f1090ffcc1ce9831dfd6a162b48e6067c
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:34 -0700

    xlsatoms: rebottle for ghcr.io

--- Formula/xlsatoms.rb
+++ Formula/xlsatoms.rb
@@ -6,12 +6,6 @@ class Xlsatoms < Formula
   mirror "https://ftp.x.org/pub/individual/app/xlsatoms-1.1.3.tar.bz2"
   sha256 "57868f958c263976727881f3078e55b86b4a109dc578d2b92f5c6d690850a382"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3a65ab7571ad039464f9fd843347ae51fe0c210ce5c6d6f6f9b14fac357ceff4"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "libxcb"
