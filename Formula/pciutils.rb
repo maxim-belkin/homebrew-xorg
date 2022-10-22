5a69ca893ac4bf19d530437589cf2a12c97483e3
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:31 -0700

    pciutils: rebottle for ghcr.io

--- Formula/pciutils.rb
+++ Formula/pciutils.rb
@@ -3,15 +3,10 @@ class Pciutils < Formula
   homepage "https://www.x.org/"
   url "https://www.kernel.org/pub/software/utils/pciutils/pciutils-3.5.1.tar.xz"
   sha256 "2bf3a4605a562fb6b8b7673bff85a474a5cf383ed7e4bd8886b4f0939013d42f"
-  # tag "linuxbrew"
 
   livecheck do
     url "https://www.kernel.org/pub/software/utils/pciutils/"
     regex(/href=.*?pciutils[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  bottle do
-    sha256 x86_64_linux: "a824aca17f0b34dcc21ea46674213d0a71d213e2bb69e7314f8d8113468cb649"
   end
 
   depends_on "pkg-config" => :build
