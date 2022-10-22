f977c1cd3fc192eb8172e17fd5dfbc63ab9d8b62
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-jis-misc: rebottle for ghcr.io

--- Formula/font-jis-misc.rb
+++ Formula/font-jis-misc.rb
@@ -7,15 +7,14 @@ class FontJisMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-jis-misc-1.0.3.tar.bz2"
   sha256 "2b18ce10b367ebafe95a17de799b6db9a24e2337188d124adaf68af05b1fac65"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
