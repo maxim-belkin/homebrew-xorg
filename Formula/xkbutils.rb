ee4029aa7bebc3e811d7b8c70458aae15d69f27e
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:34 -0700

    xkbutils: rebottle for ghcr.io

--- Formula/xkbutils.rb
+++ Formula/xkbutils.rb
@@ -6,12 +6,6 @@ class Xkbutils < Formula
   mirror "https://ftp.x.org/pub/individual/app/xkbutils-1.0.4.tar.bz2"
   sha256 "d2a18ab90275e8bca028773c44264d2266dab70853db4321bdbc18da75148130"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f7eea4644fbd505790481fefab873d63b2771f7c1772558d0478090a5b0681f9"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
