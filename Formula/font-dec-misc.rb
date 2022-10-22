1c44aeb3b62ba762f2f7edaa320c6398fa6190dc
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-dec-misc: rebottle for ghcr.io

--- Formula/font-dec-misc.rb
+++ Formula/font-dec-misc.rb
@@ -7,20 +7,14 @@ class FontDecMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-dec-misc-1.0.3.tar.bz2"
   sha256 "e19ddf8b5f8de914d81675358fdfe37762e9ce524887cc983adef34f2850ff7b"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "f175af12ffa53c6257c4257054392032bbc833ef1bd30badf6b1f538e9246918"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
