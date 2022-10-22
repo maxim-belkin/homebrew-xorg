45f8d96cfd0fa8012e0e08ddbe43e4aadf5ab1f5
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:25 -0700

    font-bitstream-type1: rebottle for ghcr.io

--- Formula/font-bitstream-type1.rb
+++ Formula/font-bitstream-type1.rb
@@ -7,19 +7,13 @@ class FontBitstreamType1 < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bitstream-type1-1.0.3.tar.bz2"
   sha256 "c6ea0569adad2c577f140328dc3302e729cb1b1ea90cd0025caf380625f8a688"
   revision 3
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "b4151dd2aaea0a42aa4848b26d245ba4eb35164e329355214430c0fd2709ce04"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
