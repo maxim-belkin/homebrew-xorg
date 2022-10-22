e0b2610bad8313c505aa51684ddd6646e03bf59c
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:25 -0700

    font-bitstream-75dpi: rebottle for ghcr.io

--- Formula/font-bitstream-75dpi.rb
+++ Formula/font-bitstream-75dpi.rb
@@ -7,20 +7,14 @@ class FontBitstream75dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bitstream-75dpi-1.0.3.tar.bz2"
   sha256 "ba3f5e4610c07bd5859881660753ec6d75d179f26fc967aa776dbb3d5d5cf48e"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "5946a64133c72265e96fdf0529e2ca7568ba3f6809ad349f7a9632364d4ab015"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
