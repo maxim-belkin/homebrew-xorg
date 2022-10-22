1ecedbe1a5b2e2aeac4a99078a6fada35497fb91
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:28 -0700

    font-sony-misc: rebottle for ghcr.io

--- Formula/font-sony-misc.rb
+++ Formula/font-sony-misc.rb
@@ -7,20 +7,14 @@ class FontSonyMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-sony-misc-1.0.3.tar.bz2"
   sha256 "2043a326ba347c9da5ca1e9bc363e2521c3ea40b43b1f9662d333efd4867cff5"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "2c8bc42becd8e7efa7becdcb3c712fa6b6ba009a8d0c41b6202aea232902506e"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
