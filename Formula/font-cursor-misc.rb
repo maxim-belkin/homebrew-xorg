609a98045d0256c65c3fecb4dd0fe556894c8ea4
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-cursor-misc: rebottle for ghcr.io

--- Formula/font-cursor-misc.rb
+++ Formula/font-cursor-misc.rb
@@ -7,20 +7,14 @@ class FontCursorMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-cursor-misc-1.0.3.tar.bz2"
   sha256 "17363eb35eece2e08144da5f060c70103b59d0972b4f4d77fd84c9a7a2dba635"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "de0f977e9ca43b831f8d93b177a4bbec4ebd71b0ec7db9cb4a289a3dad30e0af"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
