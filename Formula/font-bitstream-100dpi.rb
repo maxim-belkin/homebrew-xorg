1e14669019483e0108d8d9539aa66db497dff8a1
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:25 -0700

    font-bitstream-100dpi: rebottle for ghcr.io

--- Formula/font-bitstream-100dpi.rb
+++ Formula/font-bitstream-100dpi.rb
@@ -7,21 +7,14 @@ class FontBitstream100dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bitstream-100dpi-1.0.3.tar.bz2"
   sha256 "ebe0d7444e3d7c8da7642055ac2206f0190ee060700d99cd876f8fc9964cb6ce"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    rebuild 1
-    sha256 x86_64_linux: "e41a5038508649b3b194a54b8c1c6a2f7275ba6730f0415047834e670b6a654e"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
