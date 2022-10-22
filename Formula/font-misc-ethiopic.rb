6e4639d4ef3683ddb78c0aa84edd3c41dca43846
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:27 -0700

    font-misc-ethiopic: rebottle for ghcr.io

--- Formula/font-misc-ethiopic.rb
+++ Formula/font-misc-ethiopic.rb
@@ -7,19 +7,13 @@ class FontMiscEthiopic < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-misc-ethiopic-1.0.3.tar.bz2"
   sha256 "53cb1fd83afdbe7939c0eac34003676ee0e6023216892d98054db90b703c98a5"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "f04866931ead9aea5924f41ca48cedbd1dbabbb69b2bd69751ce8b0b0b6c398d"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
