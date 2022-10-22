cec5638d27ebafdeaa2bbc21d006cf9027332d2c
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:35 -0700

    xorg-sgml-doctools: rebottle for ghcr.io

--- Formula/xorg-sgml-doctools.rb
+++ Formula/xorg-sgml-doctools.rb
@@ -4,12 +4,6 @@ class XorgSgmlDoctools < Formula
   url "https://www.x.org/archive/individual/doc/xorg-sgml-doctools-1.11.tar.gz"
   sha256 "986326d7b4dd2ad298f61d8d41fe3929ac6191c6000d6d7e47a8ffc0c34e7426"
   revision 1
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9ee67d30d91f5bb5bd26fbec461ec6e6898f4f9b395391720c50bebd6be98e48"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "util-macros"=> :build
