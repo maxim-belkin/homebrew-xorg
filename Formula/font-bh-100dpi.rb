339bcafc194ea1e996e73292447472abda009b6b
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:24 -0700

    font-bh-100dpi: rebottle for ghcr.io

--- Formula/font-bh-100dpi.rb
+++ Formula/font-bh-100dpi.rb
@@ -7,20 +7,14 @@ class FontBh100dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bh-100dpi-1.0.3.tar.bz2"
   sha256 "23c07162708e4b79eb33095c8bfa62c783717a9431254bbf44863734ea239481"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "374519ba40f0e8907525f75b4e0876896d65f8a1036b884ea5ad113bedebd244"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
