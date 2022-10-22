c6b9e00b0358068e2358418bd7d7059fc5eb0723
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:24 -0700

    font-bh-lucidatypewriter-100dpi: rebottle for ghcr.io

--- Formula/font-bh-lucidatypewriter-100dpi.rb
+++ Formula/font-bh-lucidatypewriter-100dpi.rb
@@ -7,20 +7,14 @@ class FontBhLucidatypewriter100dpi < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-bh-lucidatypewriter-100dpi-1.0.3.tar.bz2"
   sha256 "62a83363c2536095fda49d260d21e0847675676e4e3415054064cbdffa641fbb"
   revision 2
-  # tag "linuxbrew"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-xorg"
-    sha256 x86_64_linux: "bae091b8a973a559d4837b1c8a9b58de55b779325664542477c797c76a3c7a7e"
-  end
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
