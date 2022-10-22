af2ed5ad98120a4cc63cf24f937d77f52f64cce9
Jonathan Chang <me@jonathanchang.org>
2022-10-22 13:37:26 -0700

    font-isas-misc: rebottle for ghcr.io

--- Formula/font-isas-misc.rb
+++ Formula/font-isas-misc.rb
@@ -7,15 +7,14 @@ class FontIsasMisc < Formula
   mirror "https://ftp.x.org/archive/individual/font/font-isas-misc-1.0.3.tar.bz2"
   sha256 "5824ab4b485951107dd245b8f7717d2822f1a6dbf6cea98f1ac7f49905c0a867"
   revision 2
-  # tag "linuxbrew"
 
   keg_only "part of Xorg-fonts package"
 
+  depends_on "bdftopcf" => :build
   depends_on "bzip2" => [:build, :recommended]
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
-  depends_on "linuxbrew/xorg/bdftopcf" => :build
-  depends_on "linuxbrew/xorg/mkfontscale" => :build
+  depends_on "mkfontscale" => :build
   depends_on "pkg-config" => :build
 
   def install
