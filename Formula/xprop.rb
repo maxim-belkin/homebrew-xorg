b3004f4d40b542169194c3abfd0905450c224666
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:08 +0000

    xprop: add 1.2.4_1 bottle.

--- Formula/xprop.rb
+++ Formula/xprop.rb
@@ -6,6 +6,12 @@ class Xprop < Formula
   sha256 "8c77fb096e46c60032b7e2bde9492c3ffcc18734f50b395085a5f10bfd3cf753"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xprop-1.2.4_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "30e349abc9bd5a8720025a762d2f7703812602a13aef09d8cbbc845b1fbca273"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "libx11"
