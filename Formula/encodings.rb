fbc09a99854536bff4e3ceb9c57e7f56a648979d
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:09 +0000

    encodings: add 1.0.5_1 bottle.

--- Formula/encodings.rb
+++ Formula/encodings.rb
@@ -6,6 +6,12 @@ class Encodings < Formula
   sha256 "bd96e16143a044b19e87f217cf6a3763a70c561d1076aad6f6d862ec41774a31"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/encodings-1.0.5_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8edef44b7d977fe565cfc81363eccd71f402d21751ca070a55722c50b1ccf1fd"
+  end
+
   keg_only "part of Xorg-fonts package"
 
   depends_on "font-util" => :build
