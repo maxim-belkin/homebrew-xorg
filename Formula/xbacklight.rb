56a2384378366d0756d00df25bbbc4e3ab7d79c3
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:12 +0000

    xbacklight: add 1.2.3_1 bottle.

--- Formula/xbacklight.rb
+++ Formula/xbacklight.rb
@@ -6,6 +6,12 @@ class Xbacklight < Formula
   sha256 "3a27f324777ae99fee476cfb2f064576fb8cba4eb77f97cda37adda1c1d39ade"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xbacklight-1.2.3_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b851e7beb3d4eac3d2f2197c18bb8227dff8f3b3d1249e95b89044d08211369b"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libxcb"
   depends_on "xcb-util"
