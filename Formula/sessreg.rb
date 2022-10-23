3b8a57ce374dde29d726639cb25b9de0947764af
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:09 +0000

    sessreg: add 1.1.2 bottle.

--- Formula/sessreg.rb
+++ Formula/sessreg.rb
@@ -5,6 +5,12 @@ class Sessreg < Formula
   url "https://www.x.org/pub/individual/app/sessreg-1.1.2.tar.bz2"
   sha256 "dfcb9e954273ace63879aec053c7880466f8755752a35aa9ced3fa5f04fd2c33"
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/sessreg-1.1.2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9a228b4491c2b841281547e5b55015e7f4e2a97890d14a6d6baf58c50e6d65ca"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
 
