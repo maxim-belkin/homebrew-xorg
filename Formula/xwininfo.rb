88584624c7b4c3110cc07a2fb8ba6848d0686c7a
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:08 +0000

    xwininfo: add 1.1.5_2 bottle.

--- Formula/xwininfo.rb
+++ Formula/xwininfo.rb
@@ -6,6 +6,12 @@ class Xwininfo < Formula
   sha256 "7a405441dfc476666c744f5fcd1bc8a75abf8b5b1d85db7b88b370982365080e"
   revision 2
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xwininfo-1.1.5_2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "40a443508592c5a0993b2c089ed2bc55675dc1b55edc05a50408bf0385d44492"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libx11"
   depends_on "xcb-util-wm"
