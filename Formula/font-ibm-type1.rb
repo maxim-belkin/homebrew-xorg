7f2916b6a6f8c8fc7e2d1fa53b7a67a4d6e312f5
Jonathan Chang <me@jonathanchang.org>
2022-10-23 09:42:46 -0700

    font-ibm-type1: fix root_url in bottle

--- Formula/font-ibm-type1.rb
+++ Formula/font-ibm-type1.rb
@@ -9,7 +9,7 @@ class FontIbmType1 < Formula
   revision 2
 
   bottle do
-    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/font-ibm-type1-1.0.3_2"
+    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
     rebuild 1
     sha256 cellar: :any_skip_relocation, x86_64_linux: "62a67aadbc3a616cae67a59101408be7637512fb1ed0961a61f29c4f68ebaf45"
   end
