02fee3ab4412062c4102d70c9a99a5553bd90cd0
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:12 +0000

    xorg-sgml-doctools: add 1.11_1 bottle.

--- Formula/xorg-sgml-doctools.rb
+++ Formula/xorg-sgml-doctools.rb
@@ -5,6 +5,12 @@ class XorgSgmlDoctools < Formula
   sha256 "986326d7b4dd2ad298f61d8d41fe3929ac6191c6000d6d7e47a8ffc0c34e7426"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xorg-sgml-doctools-1.11_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0565182114010a16c12ea163f76b4abfd7e120abd9283b27b32e2be7c568e01c"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros"=> :build
 
