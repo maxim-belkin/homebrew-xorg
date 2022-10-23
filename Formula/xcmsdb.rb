ce298d0254abd98ab64eb6752b5a4c4a5a4a42e2
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:08 +0000

    xcmsdb: add 1.0.5_1 bottle.

--- Formula/xcmsdb.rb
+++ Formula/xcmsdb.rb
@@ -7,6 +7,12 @@ class Xcmsdb < Formula
   sha256 "e5585361bb8b6a05bb814a8d0e444ee93e0f00180881d3070aff4571e97f67c6"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xcmsdb-1.0.5_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d33cfabfbb30b55431202931add373ce7ccc4cf56b71173ddf9dae686fc987ac"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libx11"
 
