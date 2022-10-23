f83c1d990cd25bd72d699cae144bfca904dc7f77
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:10 +0000

    xgamma: add 1.0.6_1 bottle.

--- Formula/xgamma.rb
+++ Formula/xgamma.rb
@@ -7,6 +7,12 @@ class Xgamma < Formula
   sha256 "0ef1c35b5c18b1b22317f455c8df13c0a471a8efad63c89c98ae3ce8c2b222d3"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xgamma-1.0.6_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "84071f66535c5ae388c1c7b3a975014d3bf4d449dc62beca39ab1ebaef671436"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
   depends_on "libx11"
