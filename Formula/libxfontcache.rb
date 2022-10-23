c89857001fa3846ef49f2e441baa5791824fd487
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:08 +0000

    libxfontcache: add 1.0.5_1 bottle.

--- Formula/libxfontcache.rb
+++ Formula/libxfontcache.rb
@@ -5,6 +5,12 @@ class Libxfontcache < Formula
   sha256 "fdba75307a0983d2566554e0e9effa7079551f1b7b46e8de642d067998619659"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/libxfontcache-1.0.5_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "631c84fccc8734f5e1a2cedc5fe97e5291c13db61ff58b6668915e515de8478a"
+  end
+
   option "without-test", "Skip compile-time tests"
 
   depends_on "pkg-config" => :build
