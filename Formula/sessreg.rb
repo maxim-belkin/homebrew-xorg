8a1e948f130a7382bfb380a13e6fa741e87616a1
Jonathan Chang <me@jonathanchang.org>
2022-10-23 09:42:51 -0700

    sessreg: fix root_url in bottle

--- Formula/sessreg.rb
+++ Formula/sessreg.rb
@@ -6,7 +6,7 @@ class Sessreg < Formula
   sha256 "dfcb9e954273ace63879aec053c7880466f8755752a35aa9ced3fa5f04fd2c33"
 
   bottle do
-    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/sessreg-1.1.2"
+    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
     rebuild 1
     sha256 cellar: :any_skip_relocation, x86_64_linux: "9a228b4491c2b841281547e5b55015e7f4e2a97890d14a6d6baf58c50e6d65ca"
   end
