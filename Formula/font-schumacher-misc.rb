b2c2a28adca4fbc5c200fdc8b901fa9ec45629f8
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:10 +0000

    font-schumacher-misc: add 1.1.2_2 bottle.

--- Formula/font-schumacher-misc.rb
+++ Formula/font-schumacher-misc.rb
@@ -8,6 +8,12 @@ class FontSchumacherMisc < Formula
   sha256 "e444028656e0767e2eddc6d9aca462b16a2be75a47244dbc199b2c44eca87e5a"
   revision 2
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/font-schumacher-misc-1.1.2_2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e8b4d5eb28f30602454092ec6d01a00b5a2612b0aefeb7078f09a41287921926"
+  end
+
   keg_only "part of Xorg-fonts package"
 
   depends_on "bdftopcf" => :build
