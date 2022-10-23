c3aef0e3fc1821a4411d78f844a009c3447655d5
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:11 +0000

    fontcacheproto: add 0.1.3 bottle.

--- Formula/fontcacheproto.rb
+++ Formula/fontcacheproto.rb
@@ -6,6 +6,12 @@ class Fontcacheproto < Formula
   mirror "https://ftp.x.org/pub/individual/proto/fontcacheproto-0.1.3.tar.bz2"
   sha256 "1dcaa659d416272ff68e567d1910ccc1e369768f13b983cffcccd6c563dbe3cb"
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/fontcacheproto-0.1.3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ce9b29742ba8cb25b7dffe44796bbc8388fa8060967d892346a106d818c84e86"
+  end
+
   depends_on "pkg-config" => :build
 
   def install
