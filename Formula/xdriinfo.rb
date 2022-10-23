b175b857550416b110bdebf6010a2d44a5a448f0
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:09 +0000

    xdriinfo: add 1.0.6_2 bottle.

--- Formula/xdriinfo.rb
+++ Formula/xdriinfo.rb
@@ -7,6 +7,12 @@ class Xdriinfo < Formula
   sha256 "d9ccd2c3e87899417acc9ea1f3e319a4198112babe1dc711273584f607449d51"
   revision 2
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xdriinfo-1.0.6_2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "92b47c8ba8e32bc9fbfd27be6a355ebe7d5872a04028150bc79ad7e353db21ba"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
   depends_on "libx11"
