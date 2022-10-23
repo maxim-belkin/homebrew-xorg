d2cbaa056de16f5396df0301ba5435732d5a76e8
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:10 +0000

    xlsatoms: add 1.1.3_1 bottle.

--- Formula/xlsatoms.rb
+++ Formula/xlsatoms.rb
@@ -7,6 +7,12 @@ class Xlsatoms < Formula
   sha256 "57868f958c263976727881f3078e55b86b4a109dc578d2b92f5c6d690850a382"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xlsatoms-1.1.3_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "93fa50c8d032a0857abde4cca86b58431f10d19713ae0055d251f8962e753446"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libxcb"
 
