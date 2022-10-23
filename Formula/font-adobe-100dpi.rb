13cdf7744bb096ea647de58357c69f009d454dc8
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:10 +0000

    font-adobe-100dpi: add 1.0.3_2 bottle.

--- Formula/font-adobe-100dpi.rb
+++ Formula/font-adobe-100dpi.rb
@@ -8,6 +8,12 @@ class FontAdobe100dpi < Formula
   sha256 "b2c08433eab5cb202470aa9f779efefce8d9cab2534f34f3aa4a31d05671c054"
   revision 2
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/font-adobe-100dpi-1.0.3_2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3c3668bc212d6a116d7f8640b9c8f8ec251ccd46c90a07b0849c8f45011bc451"
+  end
+
   keg_only "part of Xorg-fonts package"
 
   depends_on "bdftopcf" => :build
