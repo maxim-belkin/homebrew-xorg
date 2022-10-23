2491d205485cce09b449c01b87418d465ba608ec
Jonathan Chang <me@jonathanchang.org>
2022-10-23 06:10:09 +0000

    xwd: add 1.0.7_1 bottle.

--- Formula/xwd.rb
+++ Formula/xwd.rb
@@ -7,6 +7,12 @@ class Xwd < Formula
   sha256 "cd6815b8b9e0b98e284d3d732fb12162159cb9dcee4f45a7d4c0bd8b308a6794"
   revision 1
 
+  bottle do
+    root_url "https://github.com/maxim-belkin/homebrew-xorg/releases/download/xwd-1.0.7_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c49945f046db8b9ab5568c6f3a7d72e29dbb84125b4743f26de233902f69709b"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libx11"
   depends_on "libxkbfile"
