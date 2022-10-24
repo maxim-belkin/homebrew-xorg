88a85bfd6c9ab45635c51629493e88eca7b4c9a8
Maxim Belkin <maxim.belkin@gmail.com>
2022-10-23 23:37:32 -0700

    xcmsdb 1.0.6

--- Formula/xcmsdb.rb
+++ Formula/xcmsdb.rb
@@ -2,10 +2,9 @@ class Xcmsdb < Formula
   desc "X.Org Applications: xcmsdb"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xcmsdb-1.0.5.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xcmsdb-1.0.5.tar.bz2"
-  sha256 "e5585361bb8b6a05bb814a8d0e444ee93e0f00180881d3070aff4571e97f67c6"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xcmsdb-1.0.6.tar.xz"
+  mirror "https://ftp.x.org/pub/individual/app/xcmsdb-1.0.6.tar.xz"
+  sha256 "3c77eec4537d5942bb0966973b787bfdaf7121f3125ffa81bb1c9708d4cf4f55"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
