46ecf8496c4a34e159ff8004e10eeee4fd8be472
Maxim Belkin <maxim.belkin@gmail.com>
2022-10-25 06:18:40 -0700

    xrefresh 1.0.7

--- Formula/xrefresh.rb
+++ Formula/xrefresh.rb
@@ -2,10 +2,9 @@ class Xrefresh < Formula
   desc "X.Org Applications: xrefresh"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xrefresh-1.0.6.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xrefresh-1.0.6.tar.bz2"
-  sha256 "287dfb9bb7e8d780d07e672e3252150850869cb550958ed5f8401f0835cd6353"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xrefresh-1.0.7.tar.xz"
+  mirror "https://ftp.x.org/pub/individual/app/xrefresh-1.0.7.tar.xz"
+  sha256 "a9f1d635f2f42283d0174e94d09ab69190c227faa5ab542bfe15ed9607771b1e"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
