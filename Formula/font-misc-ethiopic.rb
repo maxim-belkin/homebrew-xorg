7ff654e870e69c3ee04225be81e86f434428edd6
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 05:36:35 -0700

    font-misc-ethiopic 1.0.4
    Closes #744.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/font-misc-ethiopic.rb
+++ Formula/font-misc-ethiopic.rb
@@ -2,11 +2,10 @@ class FontMiscEthiopic < Formula
   desc "X.Org Fonts: font misc ethiopic"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
-  url "https://www.x.org/pub/individual/font/font-misc-ethiopic-1.0.3.tar.bz2"
-  mirror "https://xorg.freedesktop.org/archive/individual/font/font-misc-ethiopic-1.0.3.tar.bz2"
-  mirror "https://ftp.x.org/archive/individual/font/font-misc-ethiopic-1.0.3.tar.bz2"
-  sha256 "53cb1fd83afdbe7939c0eac34003676ee0e6023216892d98054db90b703c98a5"
-  revision 2
+  url "https://www.x.org/pub/individual/font/font-misc-ethiopic-1.0.4.tar.bz2"
+  mirror "https://xorg.freedesktop.org/archive/individual/font/font-misc-ethiopic-1.0.4.tar.bz2"
+  mirror "https://ftp.x.org/archive/individual/font/font-misc-ethiopic-1.0.4.tar.bz2"
+  sha256 "698c0f6ddf9ca482250bdafa08293893148393bb9cb23e0a9ca313c6dbfc4ae0"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
