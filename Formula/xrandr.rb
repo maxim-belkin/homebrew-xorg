d21e8c6ee5b7b692958663ff1e6d6a59d7674ef9
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 05:42:19 -0700

    xrandr 1.5.2
    Closes #768.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/xrandr.rb
+++ Formula/xrandr.rb
@@ -2,9 +2,8 @@ class Xrandr < Formula
   desc "X.Org Applications: xrandr"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xrandr-1.5.1.tar.xz"
-  sha256 "7bc76daf9d72f8aff885efad04ce06b90488a1a169d118dea8a2b661832e8762"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xrandr-1.5.2.tar.xz"
+  sha256 "c8bee4790d9058bacc4b6246456c58021db58a87ddda1a9d0139bf5f18f1f240"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
