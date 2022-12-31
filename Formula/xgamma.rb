8e312cc34f482213d3dea78083e5d6ec4bb131de
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 05:40:51 -0700

    xgamma 1.0.7

--- Formula/xgamma.rb
+++ Formula/xgamma.rb
@@ -2,10 +2,9 @@ class Xgamma < Formula
   desc "X.Org Applications: xgamma"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xgamma-1.0.6.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xgamma-1.0.6.tar.bz2"
-  sha256 "0ef1c35b5c18b1b22317f455c8df13c0a471a8efad63c89c98ae3ce8c2b222d3"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xgamma-1.0.7.tar.gz"
+  mirror "https://ftp.x.org/pub/individual/app/xgamma-1.0.7.tar.gz"
+  sha256 "61f5ef02883d65ab464678ad3d8c5445a0ff727fe6255af90b1b842ddf77370d"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
