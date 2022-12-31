a777dd7d5989e47ea0211a80dae63322c41d7656
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 05:41:08 -0700

    xkbevd 1.1.5

--- Formula/xkbevd.rb
+++ Formula/xkbevd.rb
@@ -2,10 +2,9 @@ class Xkbevd < Formula
   desc "X.Org Applications: xkbevd"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xkbevd-1.1.4.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xkbevd-1.1.4.tar.bz2"
-  sha256 "2430a2e5302a4cb4a5530c1df8cb3721a149bbf8eb377a2898921a145197f96a"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xkbevd-1.1.5.tar.gz"
+  mirror "https://ftp.x.org/pub/individual/app/xkbevd-1.1.5.tar.gz"
+  sha256 "5d6b65a417be57e19a76277601da83271b19de6e71cb0e8821441f6fb9973c47"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
