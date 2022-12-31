0a9c523cf0eb8234b7652f21560e84c89014e1af
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 05:40:33 -0700

    xcursorgen 1.0.8

--- Formula/xcursorgen.rb
+++ Formula/xcursorgen.rb
@@ -2,10 +2,9 @@ class Xcursorgen < Formula
   desc "X.Org Applications: xcursorgen"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xcursorgen-1.0.7.tar.bz2"
-  sha256 "35b6f844b24f1776e9006c880a745728800764dbe3b327a128772b4610d8eb3d"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xcursorgen-1.0.8.tar.gz"
+  mirror "https://ftp.x.org/pub/individual/app/xcursorgen-1.0.8.tar.gz"
+  sha256 "b8bb2756918343b8bc15a4ce875e9efb6c4e7777adba088280e53dd09753b6ac"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
