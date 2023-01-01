2bff0690a08bb78b8d601ae36fd08ba651020e9b
Maxim Belkin <maxim.belkin@gmail.com>
2022-12-31 20:01:59 -0700

    xvinfo 1.1.5

--- Formula/xvinfo.rb
+++ Formula/xvinfo.rb
@@ -2,10 +2,9 @@ class Xvinfo < Formula
   desc "X.Org Applications: xvinfo"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xvinfo-1.1.4.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/app/xvinfo-1.1.4.tar.bz2"
-  sha256 "0353220d6606077ba42363db65f50410759f9815352f77adc799e2adfa76e73f"
-  revision 1
+  url "https://www.x.org/pub/individual/app/xvinfo-1.1.5.tar.gz"
+  mirror "https://ftp.x.org/pub/individual/app/xvinfo-1.1.5.tar.gz"
+  sha256 "76fdc89a4e4207d0069ae3e511b4e30a60fcf86b630d01ef56d32ba5856e76c4"
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
