3f6972219b065d7740bcbec5c7a4a7aaebb5410d
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:13:56 -0700

    font-adobe-100dpi 1.0.4

--- Formula/font-adobe-100dpi.rb
+++ Formula/font-adobe-100dpi.rb
@@ -2,12 +2,9 @@ class FontAdobe100dpi < Formula
   desc "X.Org Fonts: font adobe 100dpi"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
-  url "https://www.x.org/pub/individual/font/font-adobe-100dpi-1.0.3.tar.bz2"
-  mirror "https://xorg.freedesktop.org/archive/individual/font/font-adobe-100dpi-1.0.3.tar.bz2"
-  mirror "https://ftp.x.org/archive/individual/font/font-adobe-100dpi-1.0.3.tar.bz2"
-  sha256 "b2c08433eab5cb202470aa9f779efefce8d9cab2534f34f3aa4a31d05671c054"
+  url "https://www.x.org/pub/individual/font/font-adobe-100dpi-1.0.4.tar.xz"
+  sha256 "b67aff445e056328d53f9732d39884f55dd8d303fc25af3dbba33a8ba35a9ccf"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
