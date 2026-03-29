84c5a65634e60bed268b2b5a5d37db5a0803fed1
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:24:19 -0700

    smproxy 1.0.8
    Closes #823.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/smproxy.rb
+++ Formula/smproxy.rb
@@ -2,9 +2,8 @@ class Smproxy < Formula
   desc "X.Org Applications: smproxy"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/smproxy-1.0.7.tar.gz"
-  mirror "https://ftp.x.org/pub/individual/app/smproxy-1.0.7.tar.gz"
-  sha256 "aabd5e644512442da2bca1ce65ccee403b760a08e354b3474753ed36033e3d21"
+  url "https://www.x.org/pub/individual/app/smproxy-1.0.8.tar.gz"
+  sha256 "5186ccc0f679339325a8670a5ca15e6faf7f6e65501e1e42160e27754f085d94"
   license "MIT"
 
   bottle do
