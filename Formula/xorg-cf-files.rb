db04fb7c9594be10ca7941fbaed7b98da2775928
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:26:54 -0700

    xorg-cf-files 1.0.9

--- Formula/xorg-cf-files.rb
+++ Formula/xorg-cf-files.rb
@@ -2,10 +2,8 @@ class XorgCfFiles < Formula
   desc "X.Org Utilities: xorg-cf-files"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/util/xorg-cf-files-1.0.7.tar.bz2"
-  mirror "https://www.x.org/archive/individual/util/xorg-cf-files-1.0.7.tar.bz2"
-  mirror "https://ftp.x.org/pub/individual/util/xorg-cf-files-1.0.7.tar.bz2"
-  sha256 "74a771d5bb2189020399998dfa2329c3e038aa2e14dd3d4056144ed9a5976308"
+  url "https://www.x.org/pub/individual/util/xorg-cf-files-1.0.9.tar.xz"
+  sha256 "07716eb1fe1fd1f8a1d6588457db0101cae70cb896d49dc65978c97b148ce976"
   license "MIT"
 
   bottle do
