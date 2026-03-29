c4be9d43eb94fae5d44c4cd7c98eb23f81e714e6
Maxim Belkin <maxim.belkin@gmail.com>
2026-03-29 19:50:13 +0000

    xorg-cf-files: add 1.0.9 bottle.

--- Formula/xorg-cf-files.rb
+++ Formula/xorg-cf-files.rb
@@ -6,6 +6,11 @@ class XorgCfFiles < Formula
   sha256 "07716eb1fe1fd1f8a1d6588457db0101cae70cb896d49dc65978c97b148ce976"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a18fd8d8475d1b8ff4d27820fe816d028ac74c7b9bba349d30a81174e7e03e83"
+  end
+
   depends_on "font-util" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
