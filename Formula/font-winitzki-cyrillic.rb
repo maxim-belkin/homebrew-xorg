67288670346c799f97685946546413cc1eca6470
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:21:45 -0700

    font-winitzki-cyrillic 1.0.4
    Closes #811.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/font-winitzki-cyrillic.rb
+++ Formula/font-winitzki-cyrillic.rb
@@ -2,12 +2,9 @@ class FontWinitzkiCyrillic < Formula
   desc "X.Org Fonts: font winitzki cyrillic"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
-  url "https://www.x.org/pub/individual/font/font-winitzki-cyrillic-1.0.3.tar.bz2"
-  mirror "https://xorg.freedesktop.org/archive/individual/font/font-winitzki-cyrillic-1.0.3.tar.bz2"
-  mirror "https://ftp.x.org/archive/individual/font/font-winitzki-cyrillic-1.0.3.tar.bz2"
-  sha256 "abd13b63d02fcaec488686c23683e5cf640b43bd32f8ca22eeae6f84df0a36a0"
+  url "https://www.x.org/pub/individual/font/font-winitzki-cyrillic-1.0.4.tar.xz"
+  sha256 "3b6d82122dc14776e3afcd877833a7834e1f900c53fc1c7bb2d67c781cfa97a8"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
