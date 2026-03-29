84758316083ac3110f3002f5a70f9cd62ed28f3d
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 12:40:40 -0700

    xorg-cf-files: switch to meson build system
    Version 1.0.9 dropped autotools in favor of meson.

--- Formula/xorg-cf-files.rb
+++ Formula/xorg-cf-files.rb
@@ -6,26 +6,17 @@ class XorgCfFiles < Formula
   sha256 "07716eb1fe1fd1f8a1d6588457db0101cae70cb896d49dc65978c97b148ce976"
   license "MIT"
 
-  bottle do
-    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "fe2e51ade9b853be3527f250f2d331b5c158d585ff9c7eb8cee81d9bc50fcd8a"
-  end
-
   depends_on "font-util" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
 
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --localstatedir=#{var}
-      --disable-dependency-tracking
-      --disable-silent-rules
-    ]
-
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    system "meson", "setup", "build",
+           "-Dprefix=#{prefix}",
+           "-Dwith-config-dir=#{lib}/X11/config"
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
   end
 
   test do
