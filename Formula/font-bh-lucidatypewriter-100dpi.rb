f3c3c84948ababa7ecedb489eb91fe4cfebd38c2
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:16:40 -0700

    font-bh-lucidatypewriter-100dpi 1.0.4
    Closes #787.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/font-bh-lucidatypewriter-100dpi.rb
+++ Formula/font-bh-lucidatypewriter-100dpi.rb
@@ -2,12 +2,9 @@ class FontBhLucidatypewriter100dpi < Formula
   desc "X.Org Fonts: font bh lucidatypewriter 100dpi"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
-  url "https://www.x.org/pub/individual/font/font-bh-lucidatypewriter-100dpi-1.0.3.tar.bz2"
-  mirror "https://xorg.freedesktop.org/archive/individual/font/font-bh-lucidatypewriter-100dpi-1.0.3.tar.bz2"
-  mirror "https://ftp.x.org/archive/individual/font/font-bh-lucidatypewriter-100dpi-1.0.3.tar.bz2"
-  sha256 "62a83363c2536095fda49d260d21e0847675676e4e3415054064cbdffa641fbb"
+  url "https://www.x.org/pub/individual/font/font-bh-lucidatypewriter-100dpi-1.0.4.tar.xz"
+  sha256 "76ec09eda4094a29d47b91cf59c3eba229c8f7d1ca6bae2abbb3f925e33de8f2"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
