c62bd4147b79c19bd584d83c9b8a071d6027aa73
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:21:56 -0700

    font-xfree86-type1 1.0.5

--- Formula/font-xfree86-type1.rb
+++ Formula/font-xfree86-type1.rb
@@ -2,12 +2,9 @@ class FontXfree86Type1 < Formula
   desc "X.Org Fonts: font xfree86 type1"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
-  url "https://www.x.org/pub/individual/font/font-xfree86-type1-1.0.4.tar.bz2"
-  mirror "https://xorg.freedesktop.org/archive/individual/font/font-xfree86-type1-1.0.4.tar.bz2"
-  mirror "https://ftp.x.org/archive/individual/font/font-xfree86-type1-1.0.4.tar.bz2"
-  sha256 "caebf42aec7be7f3bd40e0f232d6f34881b853dc84acfcdf7458358701fbe34a"
+  url "https://www.x.org/pub/individual/font/font-xfree86-type1-1.0.5.tar.xz"
+  sha256 "a93c2c788a5ea1c002af7c8662cf9d9821fb1df51b8d2b2c5e0026dfdfea4837"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://ghcr.io/v2/maxim-belkin/xorg"
