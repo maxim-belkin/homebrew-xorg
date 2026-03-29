757276b211daebf989a65727952509ca923a020b
Maxim Belkin <maxim.belkin@intel.com>
2026-03-29 11:22:08 -0700

    iceauth 1.0.10

--- Formula/iceauth.rb
+++ Formula/iceauth.rb
@@ -2,9 +2,8 @@ class Iceauth < Formula
   desc "X.Org Applications: iceauth"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/iceauth-1.0.9.tar.gz"
-  mirror "https://ftp.x.org/pub/individual/app/iceauth-1.0.8.tar.gz"
-  sha256 "5ca274cf210453e7d7cf5c827a2fbc92149df83824f99a27cde17e1f20324dc6"
+  url "https://www.x.org/pub/individual/app/iceauth-1.0.10.tar.gz"
+  sha256 "f17f373c6e7bfef9cfa4c688f165dfebec7642ad7c6304c5bb3c9bc2bfcde747"
   license "MIT"
 
   bottle do
