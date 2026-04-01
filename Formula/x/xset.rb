e1980d483fa453b48e2ec9c875f23461beab190d
Maxim Belkin <maxim.belkin@gmail.com>
2026-04-01 07:20:48 +0000

    xset: add 1.2.5 bottle.

--- Formula/x/xset.rb
+++ Formula/x/xset.rb
@@ -6,6 +6,11 @@ class Xset < Formula
   sha256 "9f692d55635b3862cd63633b1222a87680ec283c7a8e8ed6dd698a3147f75e2f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "51ce9f57cef64bff437e1a05ca5a316fa62374b4b3c204b5e75835bdf4def914"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "libx11"
