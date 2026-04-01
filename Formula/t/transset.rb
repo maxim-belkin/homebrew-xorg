e44e14354f3657b82e5f3ab113a5bb7d16c4bb03
Maxim Belkin <maxim.belkin@gmail.com>
2026-04-01 07:20:49 +0000

    transset: add 1.0.4 bottle.

--- Formula/t/transset.rb
+++ Formula/t/transset.rb
@@ -6,6 +6,11 @@ class Transset < Formula
   sha256 "81a9ab74af137733aa8cb6a37f8292948526fe7ef06b859fc0ff272c437c0b38"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/maxim-belkin/xorg"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f7ead7747bb43e8cc55f6959929ca1d44d688190b96868e826b3fb9d56631154"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
