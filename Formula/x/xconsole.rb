ac6f0b4d20bd8a9cb333e794f253bd550709a70e
Maxim Belkin <maxim.belkin@gmail.com>
2026-03-31 23:33:45 -0700

    xconsole: new formula

---
+++ Formula/x/xconsole.rb
@@ -0,0 +1,32 @@
+class Xconsole < Formula
+  desc "X.Org Applications: xconsole"
+  homepage "https://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url "https://www.x.org/pub/individual/app/xconsole-1.1.0.tar.xz"
+  sha256 "0c775978cacdda76dfc8b5a97142f145a177d26220dd307866d9dd62e7391189"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "xorgproto" => :build
+  depends_on "libxaw"
+  depends_on "libxt"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate bin/"xconsole", :executable?
+  end
+end
