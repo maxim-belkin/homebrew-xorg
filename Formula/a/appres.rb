d00bc74f93e9011dd73d78f8f08afc46f05af5be
Maxim Belkin <maxim.belkin@gmail.com>
2026-03-31 23:33:37 -0700

    appres: new formula

---
+++ Formula/a/appres.rb
@@ -0,0 +1,32 @@
+class Appres < Formula
+  desc "X.Org Applications: appres"
+  homepage "https://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url "https://www.x.org/pub/individual/app/appres-1.0.7.tar.xz"
+  sha256 "1114b189239fd87a8d1db433edcb4486346d29912132b91eaeee5667f13b819f"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "xorgproto" => :build
+  depends_on "libxmu"
+  depends_on "libxt"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate bin/"appres", :executable?
+  end
+end
