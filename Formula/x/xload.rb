1677edcf40e4d1f5f249734443e02a7b927e25fc
Maxim Belkin <maxim.belkin@gmail.com>
2026-03-31 23:33:49 -0700

    xload: new formula

---
+++ Formula/x/xload.rb
@@ -0,0 +1,34 @@
+class Xload < Formula
+  desc "X.Org Applications: xload"
+  homepage "https://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url "https://www.x.org/pub/individual/app/xload-1.2.0.tar.xz"
+  sha256 "2c0fe6d624e1a25c1707e2d56407b979d6a78d64271df9ea0a89d99b81b59a80"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "xorgproto" => :build
+  depends_on "libxaw"
+  depends_on "libxext"
+  depends_on "libxmu"
+  depends_on "libxt"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate bin/"xload", :executable?
+  end
+end
