ee5f1e53c36c22d909a85b1713f27bf1dc441227
Maxim Belkin <maxim.belkin@gmail.com>
2026-03-31 23:33:41 -0700

    transset: new formula

---
+++ Formula/t/transset.rb
@@ -0,0 +1,32 @@
+class Transset < Formula
+  desc "X.Org Applications: transset"
+  homepage "https://www.x.org/"
+  ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
+  url "https://www.x.org/pub/individual/app/transset-1.0.4.tar.xz"
+  sha256 "81a9ab74af137733aa8cb6a37f8292948526fe7ef06b859fc0ff272c437c0b38"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "xorgproto" => :build
+  depends_on "libx11"
+  depends_on "libxcomposite"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_predicate bin/"transset", :executable?
+  end
+end
