7d286824b8c83f679e9bf1fe46cd07e515d35457
Maxim Belkin <maxim.belkin@gmail.com>
2026-07-13 07:03:36 +0000

    xset 1.2.6
    Closes #875.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/x/xset.rb
+++ Formula/x/xset.rb
@@ -2,8 +2,8 @@ class Xset < Formula
   desc "X.Org Applications: xset"
   homepage "https://www.x.org/"
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
-  url "https://www.x.org/pub/individual/app/xset-1.2.5.tar.xz"
-  sha256 "9f692d55635b3862cd63633b1222a87680ec283c7a8e8ed6dd698a3147f75e2f"
+  url "https://www.x.org/pub/individual/app/xset-1.2.6.tar.xz"
+  sha256 "623837349ea887bc003f01ee2e4b6b8ddd9c2774f632c6d70eead1b56306b695"
   license "MIT"
 
   bottle do
